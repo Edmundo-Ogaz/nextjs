42bcd4019b22f1dec6b2073f4cdda240fcee3b73
Edmundo Ogaz Baltierra <ext_eogazb@falabella.cl>
2023-01-12 18:31:25 -0300

    call login

--- pages/login/index.js
+++ pages/login/index.js
@@ -10,21 +10,37 @@ export default function Login() {
 	const [ error, setError ] = useState();
 
   const handleLogin = async() => {
-    console.log("process.env", process.env.NEXT_PUBLIC_NETLIFY_SERVERLESS_API)
-    const { data } = await axios
-			.get(process.env.NEXT_PUBLIC_NETLIFY_SERVERLESS_API)
-    console.log("data", data)
-    document.cookie = "user=true";
-    window.location = '/';
+    try {
+      const { data } = await fetch(
+        `${process.env.NEXT_PUBLIC_NETLIFY_SERVERLESS_API}/user/login`, 
+        {
+          method: 'POST',
+          body: JSON.stringify({email: username, password})
+        }
+      )
+      document.cookie = "user=true";
+      window.location = '/';
+    } catch(e) {
+      console.log(e.message)
+      setError(e.message)
+    }
 	};
+
+  function handleUsername(event) {
+		setUsername(event.target.value)
+	}
+
+  function handlePassword(event) {
+		setPassword(event.target.value)
+	}
 
   return (
     <div className={styles.login}>
       <div className={styles.title}>
         Login
       </div>
-      <input type="text" label="name" id="name" style={ {margin: '15px auto'} } onChange={ setUsername } />
-      <input type="password" label="password" id="password" style={ {margin: '15px auto'} } onChange={ setPassword } />
+      <input type="text" label="name" id="name" style={ {margin: '15px auto'} } onChange={ handleUsername } />
+      <input type="password" label="password" id="password" style={ {margin: '15px auto'} } onChange={ handlePassword } />
       <div className={styles['login-button']} onClick={ handleLogin } disabled={ loading }>
         {loading ? 'Loading...' : 'Log in'}
       </div>
